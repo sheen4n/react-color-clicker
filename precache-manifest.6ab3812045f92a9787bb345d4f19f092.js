3551078cda7856b6c30ec1a452bfff389905a656
Goh Sheen An <sheen.an.goh@gmail.com>
2020-03-18 16:55:07 +0800

    Updates

---
+++ precache-manifest.6ab3812045f92a9787bb345d4f19f092.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a25c6ca22ecd52ca615928d02e8d098",
+    "url": "/react-color-clicker/index.html"
+  },
+  {
+    "revision": "383f67c4f68d00382dff",
+    "url": "/react-color-clicker/static/css/main.35ab7dae.chunk.css"
+  },
+  {
+    "revision": "d734e7dd930f213b0d69",
+    "url": "/react-color-clicker/static/js/2.d0b05944.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-color-clicker/static/js/2.d0b05944.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "383f67c4f68d00382dff",
+    "url": "/react-color-clicker/static/js/main.7694429d.chunk.js"
+  },
+  {
+    "revision": "51dcd9336dc09b3922f6",
+    "url": "/react-color-clicker/static/js/runtime-main.f1839976.js"
+  }
+]);
